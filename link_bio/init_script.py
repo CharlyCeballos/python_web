b22d6c6724ec12359a6431cf74bc96f7581275eb
CharlyCeballos <charlyfaceam912@gmail.com>
2024-01-05 15:22:28 -0600

    Create init_script.py

---
+++ link_bio/init_script.py
@@ -0,0 +1,17 @@
+import os
+
+# Run it just in case venv is not created
+os.system('py -3 -m venv venv')
+os.system('pause')
+os.system('cmd /k "venv\Scripts\\activate"')
+'''
+pip install reflex
+reflex init
+reflex run
+'''
+
+# Run each command just in case venv is already created
+'''
+venv\Scripts\activate
+reflex run
+'''
