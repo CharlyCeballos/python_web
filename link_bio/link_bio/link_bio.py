068af75489816703d619e00b623c938d38877453
CharlyCeballos <charlyfaceam912@gmail.com>
2024-01-19 15:39:58 -0600

    componente

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -1,11 +1,19 @@
 import reflex as rx
+from link_bio.components.navbar import navbar
 
 class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.text('Hola Milo!', color='blue')
+    return navbar()
+    # main_component =  rx.center(
+    #     rx.text('Hola Milo!', color='green'),
+    # )
+    # return main_component
 
-app = rx.App()
+accent_color = "#202124"
+style = { "background_color": accent_color }
+
+app = rx.App(style=style)
 app.add_page(index)
 app.compile()
